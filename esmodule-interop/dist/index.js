2cb51fe337081748a998c04321e0bfad05c30d1b
Dmitrii Mazurok <tmazurok@gmail.com>
2023-07-03 23:52:23 +0300

    init es module interop

---
+++ esmodule-interop/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cjs_module_1 = require("cjs-module");
+(0, cjs_module_1.default)();
---
+++ esmodule-interop/index.ts
@@ -0,0 +1,3 @@
+import m from "cjs-module";
+
+m();
